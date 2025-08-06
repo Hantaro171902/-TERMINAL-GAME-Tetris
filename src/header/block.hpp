0631f5f2c25f8fdb3617bd543aabbd0d29c77bb5
Hantaro171902 <chaugiahan001@gmail.com>
2025-08-06 21:59:40 +0700

    add block.hpp

---
+++ src/header/block.hpp
@@ -0,0 +1,12 @@
+#pragma once 
+#inlcude <vector>
+#include <string>
+
+struct Block {
+    std::vector<std::string> data;
+    int w, h; // Width and height of the block
+
+    Block() : w(0), h(0) {}
+    Block(std::vector<std::string> shape, int width, int height) 
+        : data(std::move(shape)), w(width), h(height) {}
+};
